1c8d765210c443cc027a9bfc563c961831cad87e
j3schaue <jake.schauer@gmail.com>
2019-08-22 13:47:41 -0500

    scraping demo

--- sandbox/intro to scraping.R
+++ sandbox/intro to scraping.R
@@ -1,21 +1,57 @@
-library(rvest)
-library(tidyverse)
+# library(rvest)
+# library(tidyverse)
+# 
+# 
+# 
+# link <- "http://www.fangraphs.com/leaders.aspx?pos=p&stats=pit&lg=all&qual=y&type=0&season=2015&month=0&season1=2015&ind=0&team=&rost=&age=&fitler=&players=&page=1_10000"
+# 
+# page <- read_html(link)
+# header <- page %>% html_nodes(headernode) %>% html_text()
+# 
+# tmp <- page %>% html_nodes(tablenode) %>% html_nodes(lines) %>% html_text()
+# data <- tmp[3:length(tmp)]
+# 
+# out <- matrix(data, ncol=length(header), byrow=T)
+# colnames(out) <- header
+# colnames(out) <- sub("%", "pct", header)
+# out <- as.data.frame(out[,-1])
+# 
+# # for(v in names(out)[grep("pct", names(out))]){
+# #   out[[v]] <- recodeToPct(out[[v]])
+# # }
+# 
+# 
+# 
 
 
 
-link <- "http://www.fangraphs.com/leaders.aspx?pos=p&stats=pit&lg=all&qual=y&type=0&season=2015&month=0&season1=2015&ind=0&team=&rost=&age=&fitler=&players=&page=1_10000"
+source("./code/utilityInfielder.R")
 
-page <- read_html(link)
-header <- page %>% html_nodes(headernode) %>% html_text()
+# getting data for one season
+link <- buildLinkFG(position = "p", 
+                    stats_for = "pit", 
+                    season = 2017, 
+                    season1 = 2017)
+fg_pitchers_2017 <- makeTableFG(link)
 
-tmp <- page %>% html_nodes(tablenode) %>% html_nodes(lines) %>% html_text()
-data <- tmp[3:length(tmp)]
+dfs = list()
+for(yr in 2012:2019){
+  
+  link <- buildLinkFG(position = "p", 
+                      stats_for = "pit", 
+                      season = yr, 
+                      season1 = yr)
+  
+  tmp <- makeTableFG(link)
+  tmp$Year = yr
+  
+  dfs[[paste("y", yr)]] = tmp
+}
 
-out <- matrix(data, ncol=length(header), byrow=T)
-colnames(out) <- header
-colnames(out) <- sub("%", "pct", header)
-out <- as.data.frame(out[,-1])
+fg_pitcher_2012_2019 <- bind_rows(dfs)
 
-# for(v in names(out)[grep("pct", names(out))]){
-#   out[[v]] <- recodeToPct(out[[v]])
-# }
+write_csv(x = fg_pitcher_2012_2019, 
+          path = "./data/fg_pitcher.csv")
+
+
+
