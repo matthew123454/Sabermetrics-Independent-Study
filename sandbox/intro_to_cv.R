3fd424680a117266007c3d584ec23605b00ec4c3
j3schaue <jake.schauer@gmail.com>
2020-02-04 17:31:00 -0600

    added multiple model fits

--- sandbox/intro_to_cv.R
+++ sandbox/intro_to_cv.R
@@ -19,7 +19,7 @@ setwd("./data")
 cy <- read_csv("cy-young-pitcher-data-2005-2015.csv")
 
 cy_sub <- cy %>%
-  select(Name, Team, Year, Votepts, ERA, WHIP, K9, W, IP, SV) %>%
+  select(Name, Team, Year, Votepts, ERA, WHIP, K9, W, IP, SV, WPA, FIP) %>%
   mutate(closer = as.integer(SV > 5)) %>%
   filter(IP > 45, Votepts > 0)
 
@@ -51,7 +51,6 @@ train_kfold_err %>%
 
 
 ###---Your turn: Amend lines 44-50 so that you fit a model that includes Wins, K9, WHIP, ERA, and ERA^2 as predictors.
-
 train_kfold_err <- train_kfold %>%
   mutate(model_fit = map2(train, "Votepts ~ ERA + WHIP + W + K9 + ERA^2", fit_model), # fit your model to each fold
          fold_err = map2(model_fit, test, mse)) # get the fold errors
@@ -59,3 +58,25 @@ train_kfold_err <- train_kfold %>%
 # Average the fold errors and take the square rood
 train_kfold_err %>%
   summarize(mean_err = sqrt(mean(unlist(fold_err))))
+
+
+train_kfold_err <- train_kfold %>%
+  mutate(model_fit_1 = map2(train, "Votepts ~ ERA + WHIP + W + K9 + ERA^2", fit_model), 
+         model_fit_2 = map2(train, "Votepts ~ ERA + WHIP + W + K9 + ERA^2 + closer:ERA", fit_model), 
+         model_fit_3 = map2(train, "Votepts ~ ERA + WHIP + W + K9 + ERA^2 + WHIP^2 + WHIP^3 + SV:closer", fit_model), 
+         model_fit_4 = map2(train, "Votepts ~ poly(ERA, 3) + poly(WHIP, 2) + poly(W, 2) + poly(K9, 2) + closer", fit_model),
+         model_fit_5 = map2(train, "Votepts ~ poly(ERA, 3) + poly(WHIP, 1) + poly(WPA, 1) + poly(FIP, 1) + K9 + W + closer", fit_model),
+         fold_err_1 = map2(model_fit_1, test, mse), 
+         fold_err_2 = map2(model_fit_2, test, mse), 
+         fold_err_3 = map2(model_fit_3, test, mse), 
+         fold_err_4 = map2(model_fit_4, test, mse), 
+         fold_err_5 = map2(model_fit_5, test, mse)) %>%
+  summarize(err_1 = sqrt(mean(unlist(fold_err_1))), 
+            err_2 = sqrt(mean(unlist(fold_err_2))), 
+            err_3 = sqrt(mean(unlist(fold_err_3))), 
+            err_4 = sqrt(mean(unlist(fold_err_4))), 
+            err_5 = sqrt(mean(unlist(fold_err_5))))
+train_kfold_err
+
+
+
