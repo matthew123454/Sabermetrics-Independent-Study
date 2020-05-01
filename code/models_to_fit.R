ca10362d70f05a6ca510cdd0d2be1335496bd239
j3schaue <jake.schauer@gmail.com>
2020-05-01 12:38:43 -0500

    Fixed typos.

--- code/models_to_fit.R
+++ code/models_to_fit.R
@@ -82,7 +82,7 @@ train_kfold_err %>%
 
 ###---Your turn: Amend lines 44-50 so that you fit a model that includes Wins, K9, WHIP, ERA, and ERA^2 as predictors.
 train_kfold_err <- train_kfold %>%
-  mutate(model_fit = map2(train, "Votepts ~ ERA + WHIP + W + K9 + ERA^2", fit_model), # fit your model to each fold
+  mutate(model_fit = map2(train, "votepts ~ era + whip + w + k9 + era^2", fit_model), # fit your model to each fold
          fold_err = map2(model_fit, test, mse)) # get the fold errors
 
 # Average the fold errors and take the square rood
